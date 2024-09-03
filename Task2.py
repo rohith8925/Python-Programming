45c3e7c2d8c1464cb9616fbfe0b299b9d21a92c2
rohith8925 <rohithcse19@gmail.com>
2024-09-03 19:49:31 +0530

    Changes in task2

--- Task2.py
+++ Task2.py
@@ -3,4 +3,4 @@ Enter_Your_Age = input("Enter Your Age : ");
 if Enter_Your_Age.isdigit():
     print(f"Your age is {Enter_Your_Age}");
 else:
-    print('⚠️Please provide the valid input');
+    print('⚠️ Age must be a number');
